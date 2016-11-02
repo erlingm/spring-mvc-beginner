1b63976048c01d367f84d9a602d1a932ca79eacd
Erling Molde <erling.molde@moldesoft.no>
2016-11-02 21:58:14 +0100

    View handling. Using flash attributes with redirect.

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.packt.webstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by Erling Molde on 31.10.2016.
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping
-    public String welcome(Model model) {
-        model.addAttribute("greeting", "Welcome to Web Store!");
-        model.addAttribute("tagline", "The one and only amazing web store");
+    public String welcome(Model model, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("greeting", "Welcome to Web Store!");
+        redirectAttributes.addFlashAttribute("tagline", "The one and only amazing web store");
 
-        return "welcome";
+        return "redirect:/welcome/greeting";
     }
 
     @RequestMapping("/welcome/greeting")
