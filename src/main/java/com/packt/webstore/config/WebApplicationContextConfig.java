9a39d0992aa71aec0c4ccc9897dd4e516fbad778
Erling Molde <erling.molde@moldesoft.no>
2016-11-02 22:10:39 +0100

    View handling. Static content, adding resource handlers.

--- src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
+++ src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -31,6 +32,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
         urlPathHelper.setRemoveSemicolonContent(false);
 
         configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("/resources/images/");
     }
 
     @Bean
