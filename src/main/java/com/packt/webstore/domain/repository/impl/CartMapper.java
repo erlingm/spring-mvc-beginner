ac79d135484c3ed1f2967f02ad61161fa77a602b
Erling Molde <erling.molde@moldesoft.no>
2016-11-20 10:36:09 +0100

    Chapter 9. Give REST to your application with Ajax. Shopping cart implemented using REST. Step 8. Created a rowmapper implementation for Cart, named CartMapper.

---
+++ src/main/java/com/packt/webstore/domain/repository/impl/CartMapper.java
@@ -0,0 +1,36 @@
+package com.packt.webstore.domain.repository.impl;
+
+import com.packt.webstore.domain.Cart;
+import com.packt.webstore.domain.CartItem;
+import com.packt.webstore.service.ProductService;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by Erling Molde on 19.11.2016.
+ */
+public class CartMapper implements RowMapper<Cart> {
+    private CartItemMapper cartItemMapper;
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    public CartMapper(NamedParameterJdbcTemplate jdbcTemplate, ProductService productService) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.cartItemMapper = new CartItemMapper(productService);
+    }
+
+    @Override
+    public Cart mapRow(ResultSet rs, int rowNum) throws SQLException {
+        String id = rs.getString("ID");
+        Cart cart = new Cart(id);
+
+        String SQL = String.format("SELECT * FROM CART_ITEM WHERE CART_ID = '%s'", id);
+        List<CartItem> cartItems = jdbcTemplate.query(SQL, cartItemMapper);
+        cart.setCartItems(cartItems);
+
+        return cart;
+    }
+}
