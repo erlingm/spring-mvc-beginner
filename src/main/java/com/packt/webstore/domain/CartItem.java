bf8566f8e200196ff77ab853333a6885ca504d92
Erling Molde <erling.molde@moldesoft.no>
2016-11-20 10:33:10 +0100

    Chapter 9. Give REST to your application with Ajax. Shopping cart implemented using REST. Step 3. CartItem.

---
+++ src/main/java/com/packt/webstore/domain/CartItem.java
@@ -0,0 +1,63 @@
+package com.packt.webstore.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by Erling Molde on 19.11.2016.
+ */
+public class CartItem implements Serializable {
+    private String id;
+    private Product product;
+    private int quantity;
+    private BigDecimal totalPrice;
+
+    public CartItem(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void updateTotalPrice() {
+        this.totalPrice = this.product.getUnitPrice().multiply(new BigDecimal(this.quantity));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        CartItem cartItem = (CartItem) obj;
+
+        return id != null ? id.equals(cartItem.id) : cartItem.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
