a951685ab26656d49ff556afbe8f51c738e24cd5
Erling Molde <erling.molde@moldesoft.no>
2016-11-20 10:39:54 +0100

    Chapter 9. Give REST to your application with Ajax. Shopping cart implemented using REST. Step 12. Created an implementation of the CartService interface for the shopping cart.

---
+++ src/main/java/com/packt/webstore/service/impl/CartServiceImpl.java
@@ -0,0 +1,48 @@
+package com.packt.webstore.service.impl;
+
+import com.packt.webstore.domain.Cart;
+import com.packt.webstore.domain.repository.CartRepository;
+import com.packt.webstore.dto.CartDto;
+import com.packt.webstore.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Erling Molde on 20.11.2016.
+ */
+@Service
+public class CartServiceImpl implements CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Override
+    public void create(CartDto cartDto) {
+        cartRepository.create(cartDto);
+    }
+
+    @Override
+    public Cart read(String cartId) {
+        return cartRepository.read(cartId);
+    }
+
+    @Override
+    public void update(String cartId, CartDto cartDto) {
+        cartRepository.update(cartId, cartDto);
+    }
+
+    @Override
+    public void delete(String id) {
+        cartRepository.delete(id);
+    }
+
+    @Override
+    public void addItem(String cartId, String productId) {
+        cartRepository.addItem(cartId, productId);
+    }
+
+    @Override
+    public void removeItem(String cartId, String productId) {
+        cartRepository.removeItem(cartId, productId);
+    }
+}
