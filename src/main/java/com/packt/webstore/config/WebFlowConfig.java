cd10fdd36161b9e391751f148d6924344c950e0c
Erling Molde <erling.molde@moldesoft.no>
2016-11-26 03:03:52 +0100

    Chapter 10. Float your application with Web Flow. Implementing the checkout flow. Step 5, created web flow configuration.

---
+++ src/main/java/com/packt/webstore/config/WebFlowConfig.java
@@ -0,0 +1,45 @@
+package com.packt.webstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.webflow.config.AbstractFlowConfiguration;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
+
+/**
+ * Created by Erling Molde on 26.11.2016.
+ */
+@Configuration
+public class WebFlowConfig extends AbstractFlowConfiguration {
+
+    @Bean
+    public FlowDefinitionRegistry flowRegistry() {
+        return getFlowDefinitionRegistryBuilder()
+                .setBasePath("/WEB-INF/flows")
+                .addFlowLocationPattern("/**/*-flow.xml")
+                .build();
+    }
+
+    @Bean
+    public FlowExecutor flowExecutor() {
+        return getFlowExecutorBuilder(flowRegistry()).build();
+    }
+
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping() {
+        FlowHandlerMapping handlerMapping = new FlowHandlerMapping();
+        handlerMapping.setOrder(-1);
+        handlerMapping.setFlowRegistry(flowRegistry());
+        return handlerMapping;
+    }
+
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter() {
+        FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
+        handlerAdapter.setFlowExecutor(flowExecutor());
+        handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
+        return handlerAdapter;
+    }
+}
