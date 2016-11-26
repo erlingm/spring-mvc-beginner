38b376afe687215f4fc760feb2e1a4e12e5d4c9b
Erling Molde <erling.molde@moldesoft.no>
2016-11-26 01:40:05 +0100

    Chapter 10. Float your application with Web Flow. Step 3, class for Address.

---
+++ src/main/java/com/packt/webstore/domain/Address.java
@@ -0,0 +1,89 @@
+package com.packt.webstore.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by Erling Molde on 26.11.2016.
+ */
+public class Address implements Serializable {
+    private Long id;
+    private String doorNo;
+    private String streetName;
+    private String areaName;
+    private String state;
+    private String country;
+    private String zipCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDoorNo() {
+        return doorNo;
+    }
+
+    public void setDoorNo(String doorNo) {
+        this.doorNo = doorNo;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Address other = (Address) obj;
+
+        return id != null ? id.equals(other.id) : other.id == null;
+    }
+}
